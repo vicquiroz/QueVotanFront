894feed87b0c4833b58b85c323d228249711b910
aparra2018 <aparra2018@alu.uct.cl>
2021-08-08 18:14:22 -0400

    Avance de gráfico de prueba
    El gráfico de prueba ahora posee modificaciones de estilo
    Se creó una función vacía que posee una alerta cuando se realiza un click en los puntos

--- proyecto/src/App.jsx
+++ proyecto/src/App.jsx
@@ -1,10 +1,8 @@
-import Graficar from "./pag/grafico.jsx"
 import Prueba from "./pag/pruebaGraf.jsx"
 
 function App() {
   return (
     <div className="App">
-      <Graficar/>
       <Prueba/>
     </div>
   );
--- proyecto/src/pag/pruebaGraf.jsx
+++ proyecto/src/pag/pruebaGraf.jsx
@@ -1,27 +1,52 @@
 import React, {useRef, useEffect} from 'react';
 import {select} from 'd3';
 
-const tup = [[1,10],[2,12],[3,5],[4,40],[5,30]]
+const data = [  ["T1",100,10],
+                ["T2",200,12],
+                ["T3",300,5],
+                ["T4",400,40],
+                ["T5",500,30]]
 
 function Prueba(){
     const svgRef = useRef();
-
+    const width = 800
+    const height = 600
+    const margintop = 10
+    const marginleft = 10
+    
     useEffect(()=> {
-        const svg = select(svgRef.current);
+        const svg = select(svgRef.current)
+                    .style("background-color","rgba(10,0,0,0.5)") 
+                    .style("width",width)
+                    .style("height",height) // Pendiente cambiar esto a porcentaje
+                    .style("margin-top",margintop)
+                    .style("margin-left",marginleft);
+
         svg.selectAll("circle")
-        .data(tup)
+        .data(data)
         .join(
             enter => enter.append("circle")
                 .attr("class", "new")
+                .attr("key", value => value[0])
                 .attr("r", 5)
-                .attr("cx", value => value[0]*50)
-                .attr("cy", value => value[1])
-                .attr("stroke", "red"),
+                .attr("cx", value => value[1])
+                .attr("cy", value => value[2])
+                .attr("stroke", "red")
+                .on("click",ClickedOn)          // Corregir funcion con paso de parametros
+                .attr("fill","rgba(255,10,10,0.5)"),
             update => update.attr("class", "updated"),
             exit => exit.remove()
         );
     },[]);
-    return <svg ref={svgRef}></svg>
+
+    return(
+        <svg  ref={svgRef} className="chart">
+        </svg>
+        )
+}
+
+function ClickedOn(){
+    alert("Test")
 }
 
 export default Prueba;
