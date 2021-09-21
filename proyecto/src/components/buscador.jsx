63a8cc4765697d24d10d9e4d7d7ab583c5ac6960
Victor Quiroz <vquiroz2018@alu.uct.cl>
2021-09-21 18:37:24 -0300

    Mejora en la barra de busqueda.
    Ahora la barra da como opción al escribir los tags de las votaciones y se auto completa con lo que se elija.

--- proyecto/src/components/buscador.jsx
+++ proyecto/src/components/buscador.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Container, Input} from 'reactstrap'
+import './estilo.css'
 
 function Buscador({setBusqueda,tags}){
+
+    const [texto, setTexto] = useState();
+    const [sugerencia, setSuge] = useState();
     console.log(tags)
+
+    const enCambio = (tex) =>{
+        let coincide = [];
+        if(tex.length>0){
+            coincide = tags.filter(tag=>{
+                const tagname = tag.desc.toLowerCase();
+                return tagname.includes(tex.toLowerCase());
+            })
+        }
+        setSuge(coincide);
+        setTexto(tex)
+    }
+
+
     return(
         <Container>
                 <Input
-                    onInput={e => setBusqueda(e.target.value)}
-                    className="form-control me-2" 
+                    onChange={e => enCambio(e.target.value)}
+                    className="input" 
                     type="search" 
                     placeholder="Buscar"
+                    value={texto}
                 >
                 </Input>
+                {sugerencia && sugerencia.slice(0,10).map((sugerencia, id) =>
+                    <div className="sugerencia list-group-item" key={id}
+                     onClick={()=> setTexto(sugerencia.desc)}>
+                        {sugerencia.desc}  
+                    </div>
+                )}
         </Container>
     )
 }
--- proyecto/src/components/tabla.jsx
+++ proyecto/src/components/tabla.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Container} from 'reactstrap'
 import { Link } from "react-router-dom";
 
+
 function Tabla({busqueda}){
 
     const posts = [
