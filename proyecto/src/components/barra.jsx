4dda66086b048ed9e9b1063cf7528dde513bebdb
Antonio <aparra2018@alu.uct.cl>
2021-11-16 15:30:50 -0300

    Añadida imágenes de la carrera y departamento a la barra

--- proyecto/src/components/barra.jsx
+++ proyecto/src/components/barra.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import {Button,NavItem,Nav,Container} from 'reactstrap';
 import { Link } from "react-router-dom";
 import Logo from '../resources/Logo.webp'
+import LogoDptIng from '../resources/Logo_depto_ing_informatica.webp'
+import LogoIngCivil from '../resources/Logo_ing_civil_informatica.webp'
 import Ayuda from "./ayuda";
+import {isMobile} from 'react-device-detect'
 function Barra({origen}){
     //const toggle = () => setOpen(!dropdownOpen);
     //const [dropdownOpen, setOpen] = useState(false);
+    var ImgSize;
+    if(isMobile===true){
+        ImgSize="100px"
+    }
+    else{
+        ImgSize="150px"
+    }
     function verorigen(){
         if(origen!=="acercade") return(<Ayuda origen={origen}/>)
     }
@@ -14,9 +24,18 @@ function Barra({origen}){
                 <Nav className="navbar navbar-inverse bg-dark bg-gradient fixed-top">
                 <Container className="container-fluid">
                     <NavItem>
-                    <Link to="/" style={{ textDecoration: 'none' }} className="text-light">
-                        <span className="navbar-brand mb-0 h1 text-light"><img src={Logo} alt="logo de la pagina" width="150px"/></span>
-                    </Link>
+                        <span className="navbar-brand mb-0 h1 text-light">
+                            <Link to="/" style={{ textDecoration: 'none' }} className="text-light">
+                                <img src={Logo} alt="Logo Página" width="150px"/>
+                            </Link>
+                            <a href="https://www.inf.uct.cl/">
+                            <img src={LogoDptIng} alt="Logo Departamento" width={ImgSize}/>
+                            </a>
+                            <a href="https://facultadingenieria.uct.cl/carreras/ingenieria-civil-en-informatica/">
+                                <img src={LogoIngCivil} alt="Logo Carrera" width={ImgSize}/>
+                            </a>
+                        </span>
+                    
                     </NavItem>
                     <NavItem>
                         <div className="btn-group">
